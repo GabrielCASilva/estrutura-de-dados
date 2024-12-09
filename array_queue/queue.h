0f9217b7bb142ed13d0e5100725034f74d9058f5
GabrielCASilva <gabriel.foxcarvalho@gmail.com>
2024-12-09 08:49:07 -0300

    iniciando struct da fila

--- array_queue/queue.h
+++ array_queue/queue.h
@@ -1,4 +1,20 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+typedef struct {
+  int *arr;
+  int start;
+  int end;
+  int qtd;
+} Queue;
+
+Queue *queue_create(int len); // cria uma nova fila
+void queue_enqueue(Queue *queue, int item); // adiciona um elemento
+int queue_dequeue(Queue *queue); // remove o próximo elemento e o retorna
+int queue_peek(Queue *queue); // retorna o próximo elemento que será removido
+void queue_contains(Queue *queue, int item); // verifica se o item está na fila
+void queue_clear(Queue *queue); // remove todos os elementos
+void queue_destroy(Queue *queue); // destroi a fila
+int queue_len(Queue *queue); // retorna o tamanho da fila
+
 #endif
